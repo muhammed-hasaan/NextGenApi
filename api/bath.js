bae5f34cb831acf7e15925f1d046301aed5465f5
muahmmed-hasaan <muhammedhasaan09@gmail.com>
2025-08-05 16:39:37 -0700

    update

--- api/bath.js
+++ api/bath.js
@@ -846,9 +846,7 @@ export default async function handler(req, res) {
   // Set response headers  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Handle OPTIONS request (preflight)
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
